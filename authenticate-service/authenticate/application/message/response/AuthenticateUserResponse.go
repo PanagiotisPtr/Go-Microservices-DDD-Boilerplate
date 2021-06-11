eae38833ed63819ff9a5a9207711d14626d523fc
Panagiotis Petridis <lPanagiotisPetridisl@gmail.com>
2021-06-11 18:13:43 +0300

    Setting refresh token as a cookie instead of returning its contents

--- authenticate-service/authenticate/application/message/response/AuthenticateUserResponse.go
+++ authenticate-service/authenticate/application/message/response/AuthenticateUserResponse.go
@@ -2,8 +2,9 @@ package response
 
 import (
 	"context"
-	"encoding/json"
+	"errors"
 	"net/http"
+	"time"
 )
 
 type AuthenticateUserResponse struct {
@@ -12,5 +13,27 @@ type AuthenticateUserResponse struct {
 }
 
 func EncodeAuthenticateUserResponse(ctx context.Context, w http.ResponseWriter, response interface{}) error {
-	return json.NewEncoder(w).Encode(response)
+	authenticateUserResponse, ok := response.(AuthenticateUserResponse)
+
+	if ok == false {
+		return errors.New("Response object could not be converted to AuthenticateUserResponse")
+	}
+
+	if authenticateUserResponse.Token == "" {
+		return errors.New("Response is missing refresh token")
+	}
+
+	cookie := http.Cookie{
+		Name:     "RefreshToken",
+		Value:    authenticateUserResponse.Token,
+		Path:     "/",
+		Expires:  time.Now().Add(time.Hour*24 - time.Second),
+		HttpOnly: true,
+		Secure:   true,
+		SameSite: http.SameSiteStrictMode,
+	}
+
+	http.SetCookie(w, &cookie)
+
+	return nil
 }
